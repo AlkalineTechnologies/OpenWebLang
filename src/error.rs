65c13170e77eb3b21c11db3a20a0c54ecb5b1520
lylythechosenone <lysander.mealy@gmail.com>
2022-08-29 10:07:32 -0400

    Fix errors

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,46 @@
 use crate::lexer::LexerInput;
 use std::ops::Range;
 
-pub fn error_print(input: &mut LexerInput, span: Range<usize>, msg: &str) {
-    let mut line = String::new();
-    for (i, ch) in input.enumerate().skip(span.start) {
-        if i == span.end {
+pub fn error_print(input: LexerInput, span: Range<usize>, msg: &str) {
+    let mut line = 1;
+    let mut column = 1;
+    let mut line_begin = 0;
+    for (i, char) in input.clone().enumerate() {
+        if i == span.start {
             break;
+        }
+        if char == '\n' {
+            line += 1;
+            column = 1;
+            line_begin = i + 1;
         } else {
-            line.push(ch);
+            column += 1;
         }
     }
-    eprintln!("{}", line);
-    eprintln!("{}", msg);
+    let mut line_len = 0;
+    for char in input.clone().skip(line_begin) {
+        if char == '\n' {
+            break;
+        }
+        line_len += 1;
+    }
+    let mut print_area = String::new();
+    for char in input.skip(line_begin) {
+        if char == '\n' {
+            break;
+        }
+        print_area.push(char);
+    }
+    eprintln!("{}", print_area);
+    eprintln!(
+        "{}{}",
+        " ".repeat(column - 1),
+        "^".repeat((span.end - span.start).min(line_len - (column - 1)))
+    );
+    #[cfg(debug_assertions)]
+    eprintln!("{}:{} ({:?}): {}", line, column, span, msg);
+    #[cfg(not(debug_assertions))]
+    eprintln!("{}:{}: {}", line, column, msg);
 }
 
 macro_rules! error {
